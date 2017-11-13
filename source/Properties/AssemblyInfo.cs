641042766301369e749d97c3133ab4e3d265ad8b
Bernhard Krenz <mail@bernhard-krenz.de>
2017-11-13 21:25:58 +0100

    new build: 1.7

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.1.15")]
-[assembly: AssemblyFileVersion("1.6.1.15")]
+[assembly: AssemblyVersion("1.7.0.17")]
+[assembly: AssemblyFileVersion("1.7.0.17")]
