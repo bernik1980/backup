34d415ddfc5776f718beea20b4c9aa21a278f74c
Bernhard Krenz <mail@bernhard-krenz.de>
2017-12-05 10:39:46 +0100

    new build: 1.8

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.0.17")]
-[assembly: AssemblyFileVersion("1.7.0.17")]
+[assembly: AssemblyVersion("1.8.0.18")]
+[assembly: AssemblyFileVersion("1.8.0.18")]
